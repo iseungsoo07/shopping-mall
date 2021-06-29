53396c88a02b706f19b531d7d33bd276ef51a825
gihangShin <abc1775@naver.com>
2021-06-29 17:47:05 +0900

    로그인 하지 않을 시 cart 페이지 접근 제한

--- src/action/ShowCartAction.java
+++ src/action/ShowCartAction.java
@@ -1,6 +1,7 @@
 package action;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -14,26 +15,32 @@ import model.Cart;
 import model.Product;
 
 public class ShowCartAction implements Action {
-	
+
 	@Override
 	public ActionForward execute(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		ActionForward forward = new ActionForward();
 		HttpSession session = req.getSession();
 		CartDAO cartDAO = new CartDAO();
 		ArrayList<Cart> carts = new ArrayList<>();
-		
-		String id = (String)session.getAttribute("id");
-		System.out.println("id : "+id);
-		
-		carts = cartDAO.showC(id);
-		
-	
-		
-		session.setAttribute("carts", carts);
-		
-		forward.setRedirect(false);
-		forward.setPath("cart.jsp");
-		
+
+		String id = (String) session.getAttribute("id");
+		System.out.println("id : " + id);
+
+		if (id == null) { // if no login
+			req.setCharacterEncoding("UTF-8");
+			res.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = res.getWriter();
+
+			out.println("<script>alert('You need Login'); location.href='home.jsp'; </script>");
+		} else { // if login
+			carts = cartDAO.showC(id);
+
+			session.setAttribute("carts", carts);
+
+			forward.setRedirect(false);
+			forward.setPath("cart.jsp");
+		}
+
 		return forward;
 	}
 
